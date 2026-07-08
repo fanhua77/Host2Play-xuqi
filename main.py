2a1a2b89870c4218a5aa6728b5ed121b0cc44240
fanhua77 <154464040+fanhua77@users.noreply.github.com>
2026-07-08 15:41:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=e543b39c-eec1-43da-b925-a61685444782",
     # 添加更多链接
 ]
 
